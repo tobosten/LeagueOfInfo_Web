be52e56df9f8803d63a454557fc4a51f81f6ea97
tobosten <tobbe.osterlin@gmail.com>
2025-04-10 20:35:06 +0200

    Initial commit

---
+++ jsFiles/app.js
@@ -0,0 +1,10 @@
+"use strict";
+let movie;
+fetch(`http://www.omdbapi.com/?apikey=543c82f9&t=Bladerunner`).then(res => res.json()).then(res => {
+    movie = res;
+    console.log("Movie: ", movie);
+});
+const Button = document.getElementById("btn1");
+Button.addEventListener("click", () => {
+    console.log("Button");
+});
