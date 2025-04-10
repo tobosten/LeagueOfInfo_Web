2dbd91e827be3403af045148c276a84a733b9c9e
tobosten <tobbe.osterlin@gmail.com>
2025-04-10 21:21:27 +0200

    Added basic background

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 
-let movie: Object;
-fetch(`http://www.omdbapi.com/?apikey=543c82f9&t=Bladerunner`).then(res => res.json()).then(res => {   
-    movie = res
-    console.log("Movie: ", movie)
+let champion: Object;
+let selectedChampion = "Aatrox"
+fetch(`https://ddragon.leagueoflegends.com/cdn/15.7.1/data/en_US/champion/${selectedChampion}.json`)
+    .then(res => res.json()).then(res => {   
+
+        champion = res.data[selectedChampion]
+        console.log("Fetched champion: ", champion)
 })
+
+
 
 const Button = document.getElementById("btn1")!;
 Button.addEventListener("click", () => {
     console.log("Button")
-    
-})
+});
 
 
 
