5513d310a2c9878ee891687dfcd238021fa60675
tobosten <tobbe.osterlin@gmail.com>
2025-04-10 21:22:23 +0200

    Added background Image Added champion fetch

--- jsFiles/app.js
+++ jsFiles/app.js
@@ -1,10 +1,18 @@
 "use strict";
-let movie;
-fetch(`http://www.omdbapi.com/?apikey=543c82f9&t=Bladerunner`).then(res => res.json()).then(res => {
-    movie = res;
-    console.log("Movie: ", movie);
+let champion;
+let selectedChampion = "Aatrox";
+fetch(`https://ddragon.leagueoflegends.com/cdn/15.7.1/data/en_US/champion/${selectedChampion}.json`)
+    .then(res => res.json()).then(res => {
+    champion = res.data[selectedChampion];
+    console.log("Fetched champion: ", champion);
 });
 const Button = document.getElementById("btn1");
 Button.addEventListener("click", () => {
     console.log("Button");
 });
+var image = document.createElement("img");
+var imageParent = document.getElementById("champImage");
+image.id = "champImage";
+image.className = "class";
+image.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${selectedChampion}_0.jpg`;
+imageParent === null || imageParent === void 0 ? void 0 : imageParent.appendChild(image);
