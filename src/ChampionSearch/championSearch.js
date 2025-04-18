9520766a24df625e3008d9db30cb79037ab49f60
tobosten <tobbe.osterlin@gmail.com>
2025-04-18 22:46:12 +0200

    Search results - Fetched data now gets placed in array called 'champs'. - Added style for searchbar results.

--- src/ChampionSearch/championSearch.js
+++ src/ChampionSearch/championSearch.js
@@ -1,13 +1,35 @@
 
+let champs = [];
+getData() // Fetches champion id and puts them in 'champs' variable
 
-let champs;
 
-fetch(`https://ddragon.leagueoflegends.com/cdn/15.7.1/data/en_US/champion.json`)
-    .then(res => res.json()).then(res => {
-
-        console.log(res.data)
-        champs = res.data
-       
+async function getData() {
+    await fetch(`https://ddragon.leagueoflegends.com/cdn/15.7.1/data/en_US/champion.json`)
+    .then(res => res.json())
+    .then(res => {
+        // 170 champs total
+        for(i in res.data) {
+            champs.push(res.data[i].id)
+        }
+        console.log(champs)
+      
     })
+}
 
+
+
+const resultsBox = document.querySelector(".searchResults")
+const searchBar = document.getElementById("searchBar");
+
+/* 
+Only works when klicking Enter
+resultsBox.addEventListener("searchBar", e => {
+    console.log(e)
+})
+ */
+
+
+searchBar.onkeyup = function(){
+    console.log(searchBar.value)
+}
     
